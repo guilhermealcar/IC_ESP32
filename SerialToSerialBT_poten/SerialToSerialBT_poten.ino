cd854c695717ba1479e4bdf2745e41745d4d920f
Guilherme do Carmo <55238625+guilhermealcar@users.noreply.github.com>
2023-04-21 15:28:41 +0200

    Add files via upload

--- SerialToSerialBT_poten/SerialToSerialBT_poten.ino
+++ SerialToSerialBT_poten/SerialToSerialBT_poten.ino
@@ -9,7 +9,6 @@
 
 BluetoothSerial SerialBT;
 uint8_t byteArray[2];
-//int byteArray[2];
 
 const int potPin = 34; //GPIO 34 (D34 on ESP32)
 
@@ -18,32 +17,21 @@ char analogWord;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
+  SerialBT.begin("ESP32test"); // Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
 void loop() {
   analogValue = analogRead(potPin); //Reading value received from the potentiometer medium pin 
 
-  //byteArray[0] = highByte(analogValue);
-  //byteArray[1] = lowByte(analogValue);
-
-  //analogWord = word(byteArray[0], byteArray[1]);
-
   if (Serial.available()) {
     SerialBT.write(Serial.read());
-    //SerialBT.write(analogValue);
-    //SerialBT.write(analogWord);
-    //SerialBT.print(analogValue);
   }
   if (SerialBT.available()) {
     Serial.write(SerialBT.read());
-    //Serial.write(analogValue);
-    //Serial.print(analogValue);
   }
-  //Serial.write(analogWord);
-  
+
   SerialBT.println(analogValue);  //Show value on cellphone
   Serial.println(analogValue);  //Show value on PC
-  delay(1000);
+  delay(1);
 }
