2aada948b089996a26a24d975a0d2c082b44b006
Guilherme do Carmo <55238625+guilhermealcar@users.noreply.github.com>
2023-11-06 15:27:10 +0100

    Parte 1 - actual version with ISR

--- Parte1/AcquireSend_ad8232_filtered_2/AcquireSend_ad8232_filtered_2/AcquireSend_ad8232_filtered_2.ino
+++ Parte1/AcquireSend_ad8232_filtered_2/AcquireSend_ad8232_filtered_2/AcquireSend_ad8232_filtered_2.ino
@@ -1,22 +1,48 @@
 #include <BluetoothSerial.h>
 
-// Define the pins for the AD8232
+BluetoothSerial SerialBT;
+
 const int LO_NEG = 18;
 const int LO_POS = 19;
 const int ECG_OUT = 4;
 
-BluetoothSerial SerialBT;
-
-// Define the sampling rate
 const int SAMPLE_RATE = 8000; // samples per second
-
-// Define the sample interval
 const unsigned long SAMPLE_INTERVAL = 1000000 / SAMPLE_RATE;
 
-// Define variables to check sample rate
 const int AD8232_OUT_PIN = 4;
 unsigned long lastSampleTime = 0;
 float sampleFrequency = 0.0;
+
+void IRAM_ATTR onTimer() {
+  // This function is called from an ISR, so keep it as short as possible
+
+  // Check if leads off detection is triggered
+  bool leadsOff = (digitalRead(LO_NEG) == 1 || digitalRead(LO_POS) == 1);
+  if (leadsOff) {
+    Serial.println('!');
+    // Handle leads off condition (e.g., set a flag, ignore the sample, etc.)
+  } else {
+    // Read the value of the ECG_OUT pin and convert to voltage
+    int ecg_value = analogRead(ECG_OUT);
+    float ecg_voltage = (ecg_value / 1023.0) * 3.3 - 1.65;
+
+    // Send the ECG voltage over Bluetooth
+    SerialBT.println(ecg_voltage);
+    Serial.println(ecg_voltage);
+  }
+
+  // Calculate the sample frequency
+  unsigned long currentTime = micros();
+  sampleFrequency = 1000000.0 / (currentTime - lastSampleTime);
+
+  Serial.print("Sample Frequency: ");
+  Serial.print(sampleFrequency);
+  Serial.println(" Hz");
+
+  lastSampleTime = currentTime;
+}
+
+hw_timer_t * timer = NULL;
 
 void setup() {
   // Initialize the serial port for debugging
@@ -28,38 +54,15 @@ void setup() {
   pinMode(LO_POS, INPUT);
 
   // Initialize Bluetooth
-  SerialBT.begin("ESP32_BT"); // Set the Bluetooth name to "ESP32_BT"
+  SerialBT.begin("ESP32_BT");
+
+  // Set up the timer and attach the ISR
+  timer = timerBegin(0, 5, true); // Timer 0, prescaler 80
+  timerAttachInterrupt(timer, &onTimer, true); // Attach the ISR function
+  timerAlarmWrite(timer, SAMPLE_INTERVAL, true); // Set the alarm to trigger at SAMPLE_INTERVAL
+  timerAlarmEnable(timer); // Enable the timer alarm
 }
 
 void loop() {
-  // Check if enough time has passed for the next sample
-  if (micros() - lastSampleTime >= SAMPLE_INTERVAL) {
-    lastSampleTime = micros();
-
-    // Check if leads off detection is triggered
-    bool leadsOff = (digitalRead(LO_NEG) == 1 || digitalRead(LO_POS) == 1);
-    if (leadsOff) {
-      Serial.println('!');
-      // Handle leads off condition (e.g., set a flag, ignore sample, etc.)
-    } else {
-      // Read the value of the ECG_OUT pin and convert to voltage
-      int ecg_value = analogRead(ECG_OUT);
-      float ecg_voltage = (ecg_value / 1023.0) * 3.3 - 1.65;
-
-      // Send the ECG voltage over Bluetooth
-      SerialBT.println(ecg_voltage);
-      Serial.println(ecg_voltage);
-    }
-
-    // Calculate the sample frequency
-    unsigned long elapsedTime = micros() - lastSampleTime;
-    sampleFrequency = 1000000.0 / elapsedTime;
-
-    Serial.print("Sample Frequency: ");
-    Serial.print(sampleFrequency);
-    Serial.println(" Hz");
-
-    // Update the last sample time
-    lastSampleTime = micros();
-  }
+  // Your main loop can remain empty, as the sampling is handled by the ISR
 }
